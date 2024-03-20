8a36197ca2d081e34c16dc4b3a83a6a8fedfd143
nightmare9315 <114150660+nightmare9315@users.noreply.github.com>
2024-03-20 15:42:55 +0000

    datatype js

---
+++ basics_02/datatype.js
@@ -0,0 +1,7 @@
+let customerId = 112244558877
+const name = "Himanshu Kaushik"
+currentCity = "Gaziabad"
+var earning = 100000
+let current = null
+
+console.table([customerId,name,currentCity,earning])
